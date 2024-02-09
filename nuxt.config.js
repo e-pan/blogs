cf8b994ad95d5f45c724b20fc9b0325ed685850f
Ken Fu <ken.fu@sgs.com>
2024-02-09 10:51:49 +0800

    端口号

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,10 @@ const pkg = require('./package')
 module.exports = {
   // spa || universal
   mode: 'universal',
-
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
   /*
    ** Headers of the page
    ** 
